23d47da9c1492357a7b3ce5ef289c225e23b82fe
Nick Payne <nick@kurai.co.uk>
2013-10-18 17:16:24 +0100

    remove deprecated schema import; this should be the responsibility of the suite under test

--- docker/scripts/configure-mysql.sh
+++ docker/scripts/configure-mysql.sh
@@ -3,5 +3,3 @@
 sleep 3
 echo "Creating horde_test database..."
 /usr/bin/mysql -u root -e "CREATE DATABASE horde_test;"
-#echo "Importing /horde/schema.sql..."
-#/usr/bin/mysql -u root horde_test < /horde/schema.sql
